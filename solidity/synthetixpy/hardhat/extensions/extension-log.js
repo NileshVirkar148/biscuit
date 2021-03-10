21ad7b3e91149c449e14addea6dbf8f48bebb9fa
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2021-03-10 10:19:31 +0530

    Delete solidity/synthetixpy directory

--- solidity/synthetixpy/hardhat/extensions/extension-log.js
+++
@@ -1,8 +0,0 @@
-const { extendEnvironment } = require('hardhat/config');
-const { gray } = require('chalk');
-
-const log = (...text) => console.log(gray(...['└─> [DEBUG]'].concat(text)));
-
-extendEnvironment(hre => {
-	hre.log = log;
-});
